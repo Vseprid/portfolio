49f4c0bdb850997e512208152989240f306bcf06
Ilia Dvorchuk <vseprid@gmail.com>
2022-03-08 15:53:12 +0200

    feat: add light background in the hero

--- scripts/script.js
+++ scripts/script.js
@@ -14,7 +14,7 @@ switchLang.addEventListener('click', switchInHeader);
 
 const theme = document.querySelector('.theme');
 
-const elemsTheme = ['body' ,'.icon', '.theme', 'a', '.switch-language span'];
+const elemsTheme = ['body' ,'.icon', '.theme', 'a', '.nav','.switch-language span', '.wrapper_hero'];
 
 theme.addEventListener('click', () => {
     elemsTheme.forEach(cl => {
